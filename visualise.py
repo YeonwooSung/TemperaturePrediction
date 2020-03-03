5e61637d698f74642123b2deac4e8564d4c20543
YeonwooSung <neos960518@gmail.com>
2020-03-04 06:48:21 +0900

    Update visualisation codes

--- visualise.py
+++ visualise.py
@@ -18,28 +18,6 @@ def generate_regplot(data_df, x_df, fig_size=(12, 30), n_cols=3, n_rows=27):
         plt.subplots_adjust(wspace=None, hspace=None)
     plt.show()
 
-# Distribution graphs (histogram/bar graph) of column data
-def plotPerColumnDistribution(df, nGraphShown, nGraphPerRow):
-    nunique = df.nunique()
-    df = df[[col for col in df if nunique[col] > 1 and nunique[col] < 50]] # For displaying purposes, pick columns that have between 1 and 50 unique values
-    nRow, nCol = df.shape
-    columnNames = list(df)
-    nGraphRow = (nCol + nGraphPerRow - 1) / nGraphPerRow
-    plt.figure(num = None, figsize = (6 * nGraphPerRow, 8 * nGraphRow), dpi = 80, facecolor = 'w', edgecolor = 'k')
-    for i in range(min(nCol, nGraphShown)):
-        plt.subplot(nGraphRow, nGraphPerRow, i + 1)
-        columnDf = df.iloc[:, i]
-        if (not np.issubdtype(type(columnDf.iloc[0]), np.number)):
-            valueCounts = columnDf.value_counts()
-            valueCounts.plot.bar()
-        else:
-            columnDf.hist()
-        plt.ylabel('counts')
-        plt.xticks(rotation = 90)
-        plt.title(f'{columnNames[i]} (column {i})')
-    plt.tight_layout(pad = 1.0, w_pad = 1.0, h_pad = 1.0)
-    plt.show()
-
 
 # Correlation matrix
 def plotCorrelationMatrix(df, graphWidth, filename):
@@ -60,19 +38,26 @@ def plotCorrelationMatrix(df, graphWidth, filename):
 
 
 # Scatter and density plots
-def plotScatterMatrix(df, plotSize, textSize):
+def plotScatterMatrix(df, plotSize, textSize, annotate_plot=False):
     df = df.select_dtypes(include =[np.number]) # keep only numerical columns
+
     # Remove rows and columns that would lead to df being singular
     df = df.dropna('columns')
     df = df[[col for col in df if df[col].nunique() > 1]] # keep columns where there are more than 1 unique values
     columnNames = list(df)
-    if len(columnNames) > 10: # reduce the number of columns for matrix inversion of kernel density plots
+
+    # reduce the number of columns for matrix inversion of kernel density plots
+    if len(columnNames) > 10:
         columnNames = columnNames[:10]
+
     df = df[columnNames]
     ax = pd.plotting.scatter_matrix(df, alpha=0.75, figsize=[plotSize, plotSize], diagonal='kde')
     corrs = df.corr().values
-    for i, j in zip(*plt.np.triu_indices_from(ax, k = 1)):
-        ax[i, j].annotate('Corr. coef = %.3f' % corrs[i, j], (0.8, 0.2), xycoords='axes fraction', ha='center', va='center', size=textSize)
+
+    if annotate_plot:
+        for i, j in zip(*plt.np.triu_indices_from(ax, k = 1)):
+            ax[i, j].annotate('Corr. coef = %.3f' % corrs[i, j], (0.8, 0.2), xycoords='axes fraction', ha='center', va='center', size=textSize)
+
     plt.suptitle('Scatter and Density Plot')
     plt.show()
 
@@ -81,7 +66,7 @@ if __name__ == '__main__':
     # ignore warning messages
     warnings.filterwarnings("ignore")
 
-    max_val = 3
+    max_val = 5
 
     if len(sys.argv) < 2:
         print('Usage: python3 main.py <mode_number>')
@@ -98,12 +83,14 @@ if __name__ == '__main__':
     data_df = load_train_csv()
     chem_df = load_unique_m_csv()
 
-    x_df = data_df.drop('critical_temp', axis=1, inplace=False)
-    #TODO y_df = data_df['critical_temp']
-
     if mode == 1:
         plotCorrelationMatrix(data_df, 20, 'train.csv')
     elif mode == 2:
-        plotCorrelationMatrix(chem_df, 20, 'unique_m.csv')
+        plotScatterMatrix(data_df, 30, 10)
     elif mode == 3:
+        plotCorrelationMatrix(chem_df, 20, 'unique_m.csv')
+    elif mode == 4:
+        plotScatterMatrix(chem_df, 30, 10)
+    elif mode == 5:
+        x_df = data_df.drop('critical_temp', axis=1, inplace=False)
         generate_regplot(data_df, x_df)
