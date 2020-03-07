36589beeb0222b8e53974fb25623fab6d8fede33
YeonwooSung <neos960518@gmail.com>
2020-03-08 02:25:38 +0900

    Finalise the visualise.py file

--- visualise.py
+++ visualise.py
@@ -4,11 +4,23 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
 import sys
+import math
 from utils import load_train_csv, load_unique_m_csv
 
 
-#TODO comment (description)
-def generate_regplot(data_df, x_df, fig_size=(12, 30), n_cols=3, n_rows=27):
+
+def plotScatterMatrix(data_df, x_df, n_cols=3, n_rows=27, path='./scatter_regplot.png'):
+    """
+    Plot the scatter matrix by using regplots.
+    The generated plot will be saved as image file instead of displaying plots.
+
+    :param data_df: The dataframe
+    :param x_df:    The dataframe that contains all columns to visualise.
+    :param n_cols:  The number of columns in subplots (default = 3)
+    :param n_rows:  The number of rows in subplots (default = 27)
+    :param paths:   The file path of the generated image.
+    """
+    fig_size = (n_cols * 5, n_rows * 4)
     fig, axs = plt.subplots(figsize=fig_size, ncols=n_cols, nrows=n_rows)
     for i, feature in enumerate(x_df.columns):
         row = int(i / n_cols)
@@ -16,11 +28,22 @@ def generate_regplot(data_df, x_df, fig_size=(12, 30), n_cols=3, n_rows=27):
         #sns.regplot(x=feature, y='critical_temp', data=data_df, ax=axs[row][col])
         sns.regplot(x=feature, y='critical_temp', data=data_df, ax=axs[row][col])
         plt.subplots_adjust(wspace=None, hspace=None)
-    plt.show()
+
+    # Save plot to image file instead of displaying
+    fig.savefig(path)
 
 
 # Correlation matrix
 def plotCorrelationMatrix(df, graphWidth, filename):
+    """
+    Plot the correlation matrix by using heatmaps.
+    The generated plot will be saved as image file instead of displaying plots.
+
+    :param df:          The dataframe
+    :param graphWidth:  The width of the graph
+    :param filename:    A name of the generated image file
+    """
+
     # drop columns with NaN
     df = df.dropna('columns')
     # keep columns where there are more than 1 unique values
@@ -41,39 +64,19 @@ def plotCorrelationMatrix(df, graphWidth, filename):
     plt.gca().xaxis.tick_bottom()
     plt.colorbar(corrMat)
     plt.title(f'Correlation Matrix for {filename}', fontsize=15)
-    plt.show()
+
+    # Save plot to image file instead of displaying
+    name = filename.replace('.csv', '')
+    plt.savefig('{}.png'.format(name))
 
 
-# Scatter and density plots
-def plotScatterMatrix(df, plotSize, textSize, annotate_plot=False):
-    df = df.select_dtypes(include =[np.number]) # keep only numerical columns
-
-    # Remove rows and columns that would lead to df being singular
-    df = df.dropna('columns')
-    df = df[[col for col in df if df[col].nunique() > 1]] # keep columns where there are more than 1 unique values
-    columnNames = list(df)
-
-    # reduce the number of columns for matrix inversion of kernel density plots
-    if len(columnNames) > 10:
-        columnNames = columnNames[:10]
-
-    df = df[columnNames]
-    ax = pd.plotting.scatter_matrix(df, alpha=0.75, figsize=[plotSize, plotSize], diagonal='kde')
-    corrs = df.corr().values
-
-    if annotate_plot:
-        for i, j in zip(*plt.np.triu_indices_from(ax, k = 1)):
-            ax[i, j].annotate('Corr. coef = %.3f' % corrs[i, j], (0.8, 0.2), xycoords='axes fraction', ha='center', va='center', size=textSize)
-
-    plt.suptitle('Scatter and Density Plot')
-    plt.show()
 
 
 if __name__ == '__main__':
     # ignore warning messages
     warnings.filterwarnings("ignore")
 
-    max_val = 5
+    max_val = 4
 
     if len(sys.argv) < 2:
         print('Usage: python3 visualise.py <mode_number>')
@@ -90,16 +93,16 @@ if __name__ == '__main__':
     data_df = load_train_csv()
     chem_df = load_unique_m_csv()
 
-    #TODO df.hist() -> histogram
 
     if mode == 1:
         plotCorrelationMatrix(data_df, 20, 'train.csv')
     elif mode == 2:
-        plotScatterMatrix(data_df, 30, 10)
+        x_df = data_df.drop('critical_temp', axis=1, inplace=False)
+        plotScatterMatrix(data_df, x_df)
     elif mode == 3:
         plotCorrelationMatrix(chem_df, 20, 'unique_m.csv')
     elif mode == 4:
-        plotScatterMatrix(chem_df, 30, 10)
-    elif mode == 5:
-        x_df = data_df.drop('critical_temp', axis=1, inplace=False)
-        generate_regplot(data_df, x_df)
+        x_df = chem_df.drop('critical_temp', axis=1, inplace=False)
+        x_df = x_df.select_dtypes(include=[np.number])
+        num_of_rows = math.ceil(x_df.shape[1] / 3)
+        plotScatterMatrix(chem_df, x_df, n_cols=3, n_rows=num_of_rows, path='./scatter_unique_m.png')
